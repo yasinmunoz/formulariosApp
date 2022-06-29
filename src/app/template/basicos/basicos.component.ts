c91bf0cdd0a11f55534cea71df6411932458cf35
Yasin Muñoz <munozy@ct-ab.local>
2022-06-29 10:22:42 +0200

    Mas Forms & Cypress

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,14 +9,32 @@ import { NgForm } from '@angular/forms';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  guardar ( miFormulario: NgForm ) {
-    console.log ( miFormulario.value )
+  guardar ( ) {
+    console.log ( this.miFormulario )
   }
 
+  nombreValido(): boolean {
+    
+    const validProducto = this.miFormulario?.controls.producto?.invalid;
+    const touchedProducto = this.miFormulario?.controls.producto?.touched;
+
+    return validProducto && touchedProducto;
+  }
+
+  precioValido(): boolean {
+    
+    const touchedProducto = this.miFormulario?.controls.precio?.touched;
+    const precioUnderZero = this.miFormulario?.value.precio < 0;
+    
+    return touchedProducto && precioUnderZero;
+  }
+  
 
 }
